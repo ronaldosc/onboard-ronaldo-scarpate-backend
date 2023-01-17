185df79e2e48da741258cfae756d89a348e9545d
Ronaldo Sc <rs.quim@gmail.com>
2023-01-17 15:34:03 -0300

    fixed auto start function for server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
-import { resolvers } from "./resolver"
-import { typeDefs } from "./schema"
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { resolvers } from "./resolver";
+import { typeDefs } from "./schema";
 
 const pureServer = new ApolloServer({ typeDefs, resolvers })
 
 ;(async function () {
-  return (await startStandaloneServer(pureServer)).url
+  await startStandaloneServer(pureServer)
 })()
 
 console.log(`🚀  Servidor Apollo pronto em http://localhost:4000`)
