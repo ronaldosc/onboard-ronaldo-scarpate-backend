799cc421fec349abd1fc89fb975a9c91af1fef9c
Ronaldo Sc <rs.quim@gmail.com>
2023-01-18 11:39:44 -0300

    updating .estlintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,14 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2022, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    ecmaVersion: 2022,
+    sourceType: "module",
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"]
   },
 
   rules: {
     "prettier/prettier": "error",
-
-    "@typescript-eslint/no-floating-promises": "error",
-
-    // Possible Errors
-    "no-async-promise-executor": "error",
-    "no-await-in-loop": "error",
-    "no-misleading-character-class": "error",
-    "no-template-curly-in-string": "error",
 
     // TypeScript-ESLint (Prettier)
     "@typescript-eslint/member-delimiter-style": "off",
@@ -35,73 +27,24 @@ module.exports = {
     ],
     "@typescript-eslint/no-use-before-define": "off",
 
+    ignorePatterns: ["node_modules", "dist", ".eslintrc.js"],
 
-    "semi": "off",
-    "@typescript-eslint/semi": "error",
+    plugins: ["@typescript-eslint"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
 
+      "prettier",
+      "plugin:prettier/recommended" /* recommended listing it at last */
+    ],
 
-    /* "no-restricted-syntax": [
-      "error",
-      // ban all enums
-      {
-        "selector": "TSEnumDeclaration",
-        "message": "My reason for not using any enums at all"
-      },
-      // ban just const enums
-      {
-        "selector": "TSEnumDeclaration[const=true]",
-        "message": "My reason for not using const enums"
-      },
-      // ban just non-const enums
-      {
-        "selector": "TSEnumDeclaration:not([const=true])",
-        "message": "My reason for not using non-const enums"
-      }
-    ] */
-  },
-
-  ignorePatterns: ["node_modules", "dist", ".eslintrc.js"],
-
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    
-    "prettier",
-    "plugin:prettier/recommended" /* recommends being the last in list */
-  ],
-
-  extends: ["prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "error",
-    "arrow-body-style": "off",
-    "prefer-arrow-callback": "off"
-  },
+    extends: ["prettier"],
+    plugins: ["prettier"],
+    rules: {
+      // "prettier/prettier": "error",
+      "arrow-body-style": "off",
+      "prefer-arrow-callback": "off"
+    },
+  }
 }
-
-/*  
-
-    // Possible Errors
-    "no-async-promise-executor": "error",
-    "no-await-in-loop": "error",
-    "no-misleading-character-class": "error",
-    "no-template-curly-in-string": "error",
-
-    // TypeScript-ESLint (Prettier)
-    "@typescript-eslint/member-delimiter-style": "off",
-
-    // TypeScript-ESLint
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/indent": "off",
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-object-literal-type-assertion": "off",
-    "@typescript-eslint/no-this-alias": [
-      "error",
-      {
-        "allowDestructuring": true
-      }
-      ],
-    "@typescript-eslint/no-use-before-define": "off"
-*/
